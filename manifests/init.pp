172df89a9dde598b6f3ab89a5563561a84c2f2e3
David Newton <david@davidjnewton.com>
2022-10-07 11:47:12 +0100

    helpful message on fail for influxdb host

--- manifests/init.pp
+++ manifests/init.pp
@@ -81,7 +81,12 @@ class influxdb (
 ) {
   # We can only manage repos, packages, services, etc on the node we are compiling a catalog for
   unless $host == $facts['networking']['fqdn'] or $host == 'localhost' {
-    fail("Unable to manage InfluxDB installation on host ${facts['networking']['fqdn']}")
+    fail(
+      @("MSG")
+        Unable to manage InfluxDB installation on host ${facts['networking']['fqdn']}
+        - We can only manage repos, packages, services, etc on the node we are compiling a catalog for.
+      | MSG
+    )
   }
 
   # If managing SSL, install the package before managing files under /etc/influxdb in order to ensure the directory exists
