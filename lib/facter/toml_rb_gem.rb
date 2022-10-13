827dce9ae7b63a26f20f52447510bf12a7da01e2
David Newton <david@davidjnewton.com>
2022-10-13 21:24:49 +0100

    generic check for toml_rb gem version

--- lib/facter/toml_rb_gem.rb
+++ lib/facter/toml_rb_gem.rb
@@ -2,6 +2,6 @@ Facter.add(:toml_rb_gem) do
   confine kernel: 'Linux'
 
   setcode do
-    { gem_installed: File.directory?('/opt/puppetlabs/server/data/puppetserver/jruby-gems/gems/toml-rb-2.1.1/') }
+    { gem_installed: (Facter::Core::Execution.execute('/opt/puppetlabs/bin/puppetserver gem list toml-rb -i') == "true") }
   end
 end
