a3446030244d0416fb875c4813163c8ac4055423
David Newton <david@davidjnewton.com>
2022-10-13 21:35:11 +0100

    single quotes

--- lib/facter/toml_rb_gem.rb
+++ lib/facter/toml_rb_gem.rb
@@ -2,6 +2,6 @@ Facter.add(:toml_rb_gem) do
   confine kernel: 'Linux'
 
   setcode do
-    { gem_installed: (Facter::Core::Execution.execute('/opt/puppetlabs/bin/puppetserver gem list toml-rb -i') == "true") }
+    { gem_installed: (Facter::Core::Execution.execute('/opt/puppetlabs/bin/puppetserver gem list toml-rb -i') == 'true') }
   end
 end
