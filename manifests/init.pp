20b126c612e96b8436f0114158c7e140eaed745b
Adrian <sensei.loafage@gmail.com>
2022-10-31 09:57:03 -0700

    Use Stdlib::Port::Unprivileged instead of Integer

--- manifests/init.pp
+++ manifests/init.pp
@@ -52,7 +52,7 @@
 class influxdb (
   # Provided by module data
   String  $host,
-  Integer $port,
+  Stdlib::Port::Unprivileged $port,
   String  $initial_org,
   String  $initial_bucket,
   String  $repo_gpg_key_id,
